79fc610b9f3c4c5ac4f089642839e52da36048b1
Sergey34 <Sergey3440@gmail.com>
2019-11-30 17:07:40 +0400

    manual job creation

--- es-join-service/src/main/kotlin/seko/es/join/service/services/JoinService.kt
+++ es-join-service/src/main/kotlin/seko/es/join/service/services/JoinService.kt
@@ -3,7 +3,10 @@ package seko.es.join.service.services
 import org.elasticsearch.client.RestHighLevelClient
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -14,11 +17,20 @@ class JoinService @Autowired constructor(val restHighLevelClient: RestHighLevelC
     lateinit var jobLauncher: JobLauncher
 
     @Autowired
-    lateinit var job: Job
+    lateinit var jobs: JobBuilderFactory
+
+    @Autowired
+    lateinit var s1: Step
 
     @Scheduled(cron = "0 * * * * ?")
     @Throws(Exception::class)
     fun perform() {
+        val job: Job = jobs["importUserJob_manual"]
+            .incrementer(RunIdIncrementer())
+            .flow(s1)
+            .end()
+            .build()
+
         val params = JobParametersBuilder()
             .addString("JobID", System.currentTimeMillis().toString())
             .toJobParameters()
