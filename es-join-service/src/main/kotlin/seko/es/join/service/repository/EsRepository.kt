78cd9efeb624f3abd8833206b802555831fcd8f5
Sergey34 <Sergey3440@gmail.com>
2019-12-07 13:16:08 +0400

    add load job configs from es

--- es-join-service/src/main/kotlin/seko/es/join/service/repository/EsRepository.kt
+++ es-join-service/src/main/kotlin/seko/es/join/service/repository/EsRepository.kt
@@ -2,21 +2,63 @@ package seko.es.join.service.repository
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import org.elasticsearch.action.search.*
+import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
+import org.elasticsearch.common.unit.TimeValue
+import org.elasticsearch.index.query.QueryBuilders.matchAllQuery
+import org.elasticsearch.search.Scroll
+import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.slice.SliceBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 import seko.es.join.service.domain.JobConfig
+import seko.es.join.service.services.config.parsing.JobConfigParser
 import java.io.File
+
 
 @Repository
 class EsRepository @Autowired constructor(
     private val restHighLevelClient: RestHighLevelClient,
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val jobConfigParser: JobConfigParser
 ) {
 
-    fun getJobs(): List<JobConfig> {
-        val jobs = objectMapper.readValue<List<JobConfig>>(File("t3.json").reader())
-        return jobs
+    fun getConfig(slice: SliceBuilder): List<JobConfig> {
+        val scroll = Scroll(TimeValue.timeValueMinutes(1L))
+        val searchRequest = SearchRequest(".join")
+        searchRequest.scroll(scroll)
+        val searchSourceBuilder = SearchSourceBuilder()
+        searchSourceBuilder.query(matchAllQuery())
+        searchSourceBuilder.slice(slice)
+        searchRequest.source(searchSourceBuilder)
+
+        var searchResponse: SearchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT)
+        var scrollId = searchResponse.scrollId
+        var searchHits = searchResponse.hits.hits
+
+        val configs = mutableListOf<JobConfig>()
+        while (searchHits != null && searchHits.isNotEmpty()) {
+            configs.addAll(searchHits.map { jobConfigParser.parse(it.sourceAsString) })
+            val scrollRequest = SearchScrollRequest(scrollId)
+            scrollRequest.scroll(scroll)
+            searchResponse = restHighLevelClient.scroll(scrollRequest, RequestOptions.DEFAULT)
+            scrollId = searchResponse.scrollId
+            searchHits = searchResponse.hits.hits
+        }
+
+        try {
+            val clearScrollRequest = ClearScrollRequest()
+            clearScrollRequest.addScrollId(scrollId)
+            val clearScrollResponse: ClearScrollResponse =
+                restHighLevelClient.clearScroll(clearScrollRequest, RequestOptions.DEFAULT)
+            val succeeded = clearScrollResponse.isSucceeded
+        } finally {
+            return configs
+        }
     }
 
+    fun getJobs(): List<JobConfig> {
+        return objectMapper.readValue(File("t3.json").reader())
+    }
 }
