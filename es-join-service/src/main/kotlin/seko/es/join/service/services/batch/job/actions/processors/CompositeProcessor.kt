43e60be585edffcdc587c6eaa8338d2c58ac5727
seko0716 <sergey.kosov@netcracker.com>
2020-01-13 17:01:47 +0400

    add sample for access to JobExecution context in processors readers and writers

--- es-join-service/src/main/kotlin/seko/es/join/service/services/batch/job/actions/processors/CompositeProcessor.kt
+++ es-join-service/src/main/kotlin/seko/es/join/service/services/batch/job/actions/processors/CompositeProcessor.kt
@@ -1,15 +1,25 @@
 package seko.es.join.service.services.batch.job.actions.processors
 
+import org.springframework.batch.core.JobExecution
+import org.springframework.batch.core.StepExecution
+import org.springframework.batch.core.annotation.BeforeStep
 import org.springframework.batch.item.ItemProcessor
 import seko.es.join.service.domain.Item
 
 class CompositeProcessor constructor(
     private val processors: List<ItemProcessor<Item, Item>>
 ) : ItemProcessor<Item, Item?> {
+    private lateinit var jobExecution: JobExecution // add custom field to global job context
+
     override fun process(item: Item): Item? {
         for (processor in processors) {
             processor.process(item) ?: return null
         }
         return item
+    }
+
+    @BeforeStep
+    fun beforeStep(stepExecution: StepExecution) {
+        jobExecution = stepExecution.jobExecution
     }
 }
