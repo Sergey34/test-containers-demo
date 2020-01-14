d924020bbc3e6d613a1736ee894c69791d063298
seko0716 <sergey.kosov@netcracker.com>
2020-01-14 09:07:26 +0400

    clean comments

--- es-join-service/src/main/kotlin/seko/es/join/service/EsJoinServiceApplication.kt
+++ es-join-service/src/main/kotlin/seko/es/join/service/EsJoinServiceApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 /**
- * обработка ошибок из балков
  * добавиь faultTolerant() и настроить политику пропусков
  * регистрация в зукипере (добавить либу)
  * подписка на znode в зукипере (добавит В либу)
